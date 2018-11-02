92527df1d1a2fbee90afa6e75fa9433df5afe0ea
alexander figueroa <afigueroa@digitalmeat.cl>
2018-11-02 03:05:25 -0300

    [General] Order & icon.

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,57 +1,59 @@
+<li class="{{ Request::is('travelers*') ? 'active' : '' }}">
+    <a href="{!! route('travelers.index') !!}"><i class="fa fa-suitcase"></i><span>Viajeros</span></a>
+</li>
+
 <li>
-    <a href="#" style="margin-bottom: -10px"><i class="fa fa-edit"></i><span>Parámetros generales</span></a>
+    <a href="#" style="margin-bottom: -10px"><i class="fa fa-cogs"></i><span>Parámetros generales</span></a>
     <ul class="menu-ul">
         <li class="{{ Request::is('genders*') ? 'active' : '' }}">
-            <a href="{!! route('genders.index') !!}"><i class="fa fa-edit"></i> <span>Géneros</span></a>
+            <a href="{!! route('genders.index') !!}"><i class="fa fa-cog"></i> <span>Géneros</span></a>
         </li>
         <li class="{{ Request::is('countries*') ? 'active' : '' }}">
-            <a href="{!! route('countries.index') !!}"><i class="fa fa-edit"></i> <span>Países</span></a>
+            <a href="{!! route('countries.index') !!}"><i class="fa fa-cog"></i> <span>Países</span></a>
         </li>
         <li class="{{ Request::is('cities*') ? 'active' : '' }}">
-            <a href="{!! route('cities.index') !!}"><i class="fa fa-edit"></i> <span>Ciudades</span></a>
+            <a href="{!! route('cities.index') !!}"><i class="fa fa-cog"></i> <span>Ciudades</span></a>
         </li>
         <li class="{{ Request::is('languages*') ? 'active' : '' }}">
-            <a href="{!! route('languages.index') !!}"><i class="fa fa-edit"></i> <span>Idiomas</span></a>
+            <a href="{!! route('languages.index') !!}"><i class="fa fa-cog"></i> <span>Idiomas</span></a>
         </li>
         <li class="{{ Request::is('months*') ? 'active' : '' }}">
-            <a href="{!! route('months.index') !!}"><i class="fa fa-edit"></i> <span>Months</span></a>
+            <a href="{!! route('months.index') !!}"><i class="fa fa-cog"></i> <span>Months</span></a>
         </li>
 
+
+        <li class="{{ Request::is('hostelActivities*') ? 'active' : '' }}">
+            <a href="{!! route('hostelActivities.index') !!}"><i class="fa fa-cog"></i><span>Hostel Activities</span></a>
+        </li>
+        <li class="{{ Request::is('hostelOffers*') ? 'active' : '' }}">
+            <a href="{!! route('hostelOffers.index') !!}"><i class="fa fa-cog"></i><span>Hostel Offers</span></a>
+        </li>
+        <li class="{{ Request::is('hostelMonths*') ? 'active' : '' }}">
+            <a href="{!! route('hostelMonths.index') !!}"><i class="fa fa-cog"></i><span>Hostel Months</span></a>
+        </li>
     </ul>
 </li>
 
-<li class="{{ Request::is('travelers*') ? 'active' : '' }}">
-    <a href="{!! route('travelers.index') !!}"><i class="fa fa-edit"></i><span>Viajeros</span></a>
-</li>
 
 <li class="{{ Request::is('hostels*') ? 'active' : '' }}">
-    <a href="{!! route('hostels.index') !!}"><i class="fa fa-edit"></i><span>Hostales</span></a>
+    <a href="{!! route('hostels.index') !!}"><i class="fa fa-hotel"></i><span>Hostales</span></a>
 </li>
 
 <li class="{{ Request::is('needActivities*') ? 'active' : '' }}">
-    <a href="{!! route('needActivities.index') !!}"><i class="fa fa-edit"></i><span>Actividades</span></a>
+    <a href="{!! route('needActivities.index') !!}"><i class="fa fa-list-ol"></i><span>Actividades</span></a>
 </li>
 
 <li class="{{ Request::is('offers*') ? 'active' : '' }}">
     <a href="{!! route('offers.index') !!}"><i class="fa fa-edit"></i><span>Ofrecimientos</span></a>
 </li>
-<li class="{{ Request::is('hostelActivities*') ? 'active' : '' }}">
-    <a href="{!! route('hostelActivities.index') !!}"><i class="fa fa-edit"></i><span>Hostel Activities</span></a>
-</li>
-
-<li class="{{ Request::is('hostelOffers*') ? 'active' : '' }}">
-    <a href="{!! route('hostelOffers.index') !!}"><i class="fa fa-edit"></i><span>Hostel Offers</span></a>
-</li>
 
 <li class="{{ Request::is('images*') ? 'active' : '' }}">
-    <a href="{!! route('images.index') !!}"><i class="fa fa-edit"></i><span>Images</span></a>
+    <a href="{!! route('images.index') !!}"><i class="fa fa-image"></i><span>Images</span></a>
 </li>
 
 
 
-<li class="{{ Request::is('hostelMonths*') ? 'active' : '' }}">
-    <a href="{!! route('hostelMonths.index') !!}"><i class="fa fa-edit"></i><span>Hostel Months</span></a>
-</li>
+
 
 <style>
     .menu-ul li{
--- routes/web.php
+++ routes/web.php
@@ -22,40 +22,21 @@ Route::match(['get', 'post'], 'register', function(){
     return redirect('/');
 });
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', 'HomeController@index');
 
-Route::resource('countries', 'CountryController');
-
-Route::resource('pruebas', 'PruebaController');
-
-Route::resource('languages', 'LanguageController');
-
-Route::resource('travelers', 'TravelerController');
-
-Route::resource('genders', 'GenderController');
-
-Route::resource('cities', 'CityController');
-
-Route::resource('needActivities', 'NeedActivityController');
-
-Route::resource('offers', 'OfferController');
-
-Route::resource('hostels', 'HostelController');
-
-Route::resource('hostels', 'HostelController');
-
-Route::resource('genders', 'GenderController');
-
-Route::resource('hostels', 'HostelController');
-
-Route::resource('hostelActivities', 'HostelActivityController');
-
-Route::resource('hostelOffers', 'HostelOfferController');
-
-Route::resource('images', 'ImagesController');
-
-Route::resource('months', 'MonthController');
-
-Route::resource('hostelMonths', 'HostelMonthController');
-
-Route::resource('genders', 'GenderController');
+    Route::resource('countries', 'CountryController');
+    Route::resource('pruebas', 'PruebaController');
+    Route::resource('languages', 'LanguageController');
+    Route::resource('travelers', 'TravelerController');
+    Route::resource('cities', 'CityController');
+    Route::resource('needActivities', 'NeedActivityController');
+    Route::resource('offers', 'OfferController');
+    Route::resource('hostels', 'HostelController');
+    Route::resource('hostelActivities', 'HostelActivityController');
+    Route::resource('hostelOffers', 'HostelOfferController');
+    Route::resource('images', 'ImagesController');
+    Route::resource('months', 'MonthController');
+    Route::resource('hostelMonths', 'HostelMonthController');
+    Route::resource('genders', 'GenderController');
+});
