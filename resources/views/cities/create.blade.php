4ff8857b09eb5566ab1291021fe9f534760bd9e8
alexander figueroa <afigueroa@digitalmeat.cl>
2018-11-06 00:28:56 -0300

    [City] Add description e image.

--- resources/views/cities/create.blade.php
+++ resources/views/cities/create.blade.php
@@ -12,7 +12,7 @@
 
             <div class="box-body">
                 <div class="row">
-                    {!! Form::open(['route' => 'cities.store']) !!}
+                    {!! Form::open(['route' => 'cities.store', 'enctype' => 'multipart/form-data']) !!}
 
                         @include('cities.fields')
 
