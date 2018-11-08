0d3e53e3632680a3fa1718ad62a36c8551d8b159
alexander figueroa <afigueroa@digitalmeat.cl>
2018-11-07 22:19:48 -0300

    [Destinations] paginate

--- app/Http/Controllers/API/DestinationsAPIController.php
+++ app/Http/Controllers/API/DestinationsAPIController.php
@@ -11,7 +11,7 @@ use Response;
 class DestinationsAPIController extends Controller{
 
     public function index(){
-        $destinations = Destination::all();
+        $destinations = Destination::paginate(6);
         foreach ($destinations as $destination){
             $destination->city = City::find($destination->city_id)->load('media');
             unset($destination->city_id);
