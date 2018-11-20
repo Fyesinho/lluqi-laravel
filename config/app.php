56c879500b0d30cf5da1b12fcb970266f3562e2d
alexander figueroa <afigueroa@digitalmeat.cl>
2018-11-20 15:10:58 -0300

    [App] Remove excel.

--- config/app.php
+++ config/app.php
@@ -173,7 +173,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Spatie\MediaLibrary\MediaLibraryServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -246,7 +245,6 @@ return [
         'Form'      => Collective\Html\FormFacade::class,
         'Html'      => Collective\Html\HtmlFacade::class,
         'Flash'     => Laracasts\Flash\Flash::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
